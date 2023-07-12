5f005e4f2c2f70dd246c352d6cd76357b925848d
Giovaneroxo <giovaneroxo@gmail.com>
2023-07-12 13:44:41 -0300

    iniciando projeto

--- src/main/java/com/br/itdep/ITdepApplication.java
+++ src/main/java/com/br/itdep/ITdepApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ITdepApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ITdepApplication.class, args);
     }
 
