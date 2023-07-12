84bd7e3f5000da2db487a8588e970d3b9820026a
Giovaneroxo <giovaneroxo@gmail.com>
2023-07-12 17:47:24 -0300

    fixing some problems

--- src/test/java/com/br/itdep/ITdepApplicationTests.java
+++ src/test/java/com/br/itdep/ITdepApplicationTests.java
@@ -1,13 +1,3 @@
 package com.br.itdep;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ITdepApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
