d81f485fe115668b55b902cd10e2a6dd7cacd2c5
Giovaneroxo <giovaneroxo@gmail.com>
2023-07-12 12:44:09 -0300

    montando projeto

---
+++ src/main/java/com/br/itdep/ITdepApplication.java
@@ -0,0 +1,13 @@
+package com.br.itdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITdepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITdepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/itdep/ITdepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.itdep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITdepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
